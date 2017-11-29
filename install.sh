793d37de92bb722d067e084fcba5d2f72ac16562
Pablo Cerda <pablo@citla.com>
2017-11-29 13:17:15 +0100

    Started an initial version of install script that will automate the installation of external dependencies that VIM will interacts with via system calls or async vimproc.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Some VIM features depend on external tools that are used via
+# system calls, here we attempt to automate the installation
+# of those dependencies.
+
+# NOTE: Drupal code is only compatible with squizlabs/php_codesniffer: >=2.8.1 <3.0
+# so in case there is a recent installed version will cause a conflict.
+# Install Drupal Coder that provides Drupal coding standard.
+composer global require drupal/coder:^8.2.12
+# Install PHP_CodeSniffer coding standards (rulesets).
+composer global require dealerdirect/phpcodesniffer-composer-installer
+
+# TODO: Handle a validation to check if there is a installed coder_sniffer.
+# TODO: Automatically add composer directory to PATH environment var.
+#export PATH="$PATH:$HOME/.composer/vendor/bin"
+
+# TODO: Handle installation of bash completion which is very useful complement.
+# stecman/composer-bash-completion-plugin
+# bamarni/symfony-console-autocomplete
