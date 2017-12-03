640f08c2ec2bd4c0a136243ef3faa4fc8e0a1503
Pablo Cerda <pablo@citla.com>
2017-12-03 01:13:55 +0100

    Ensure that PHP namespaces and traits are included on ctags file and that aliases are ignored to avoid confusion of vim-phpcomplete on classes lookup.

--- scripts/build_php_tags.sh
+++ scripts/build_php_tags.sh
@@ -3,4 +3,4 @@
 # Remove existing tags file.
 rm -rf .tags_php
 # Create the PHP tags including Drupal custom PHP extensions.
-ctags -R -o .tags_php --fields=+aimlS --langmap="php:+.inc.module.install.php" --languages=php --links=no --totals=yes --sort=yes --output-format=e-ctags
+ctags -R -o .tags_php --fields=+animltS --kinds-php=-a --langmap="php:+.inc.module.install.php" --languages=php --links=no --totals=yes --sort=yes --output-format=e-ctags
